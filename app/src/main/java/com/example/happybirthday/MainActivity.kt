eefb26cc0134e431cede4eb3f402db7a0c21749c
Nipun Sangeeth <sangeethnipun385@gmail.com>
2025-11-27 16:08:09 +0530

    setup birthday text in the screen

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+import org.jetbrains.annotations.TestOnly
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )  {
+                    GreetingText( "Happy Birthday Sam! ","Nipun",
+                        modifier= Modifier.padding(8.dp)
+                    )
+
+
+
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 50.sp,
+            modifier= Modifier
+                .padding(16.dp)
+                .align (alignment = Alignment.End)
+        )
+    }
+
+}
+
+
+
+@Composable
+fun GreetingImage(message: String, from: String,modifier: Modifier= Modifier){
+
+
+}
+
+@Preview(showBackground = true,
+    name ="My preview" )
+@Composable
+fun HappyBirthdayPreview() {
+    HappyBirthdayTheme {
+        GreetingText( "Happy Birthday Geeth! ","Nipun")
+
+
+    }
+}
